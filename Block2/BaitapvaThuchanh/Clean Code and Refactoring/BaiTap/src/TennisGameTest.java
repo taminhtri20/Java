07bf832aaa093f322588581e9701ab75467f8f53
taminhtri20 <114322816+taminhtri20@users.noreply.github.com>
2023-05-31 08:50:00 +0700

    Add files via upload

---
+++ Block2/BaitapvaThuchanh/Clean Code and Refactoring/BaiTap/src/TennisGameTest.java
@@ -0,0 +1,6 @@
+public class TennisGameTest {
+    public static void main(String[] args) {
+        TennisGame tennisGame = new TennisGame();
+        System.out.println(tennisGame.getScore(1,2));
+    }
+}
